c51d9d07d8c2e32348e160aea7c737d641fb35e6
Anton Chebotaev <anton.chebotaev@gmail.com>
2011-06-05 06:44:33 +0400

    Roles added to ImdbPersonsParser and PersonService

--- ii-service/src/main/java/com/manymonkeys/service/cinema/PersonService.java
+++ ii-service/src/main/java/com/manymonkeys/service/cinema/PersonService.java
@@ -3,7 +3,9 @@ package com.manymonkeys.service.cinema;
 import com.manymonkeys.core.ii.InformationItem;
 import me.prettyprint.hector.api.Keyspace;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.NoSuchElementException;
 
 /**
@@ -19,8 +21,10 @@ public class PersonService extends TagService {
     public static final String PERSON_FIRST_NAME = CLASS_MARK_KEY + ".PERSON_FIRST_NAME";
     public static final String PERSON_LAST_NAME = CLASS_MARK_KEY + ".PERSON_LAST_NAME";
 
-    public static final String SEARCH_KEY = PersonService.class.getName();
-    public static final String SEARCH_FORMAT = "%s#%s"; // This indicates that item was created through PersonService class
+    private static final String SEARCH_KEY = PersonService.class.getName();
+    private static final String SEARCH_FORMAT = "%s#%s"; // This indicates that item was created through PersonService class
+
+    private static final String ROLES_KEY = CLASS_MARK_KEY + ".ROLES";
 
     private static final String FULL_NAME_FORMAT = "%s %s";
 
@@ -45,6 +49,23 @@ public class PersonService extends TagService {
         } catch (NoSuchElementException e) {
             return null;
         }
+    }
+
+    public Collection<String> getRoles(InformationItem person) {
+        String rolesRaw = person.getMeta(ROLES_KEY);
+        if (rolesRaw == null)
+            return Collections.emptySet();
+
+        String[] roles = rolesRaw.split("###");
+        if (roles.length == 0)
+            return Collections.emptySet();
+
+        return Arrays.asList(roles);
+    }
+
+    public void addRole(InformationItem person, String role) {
+        String rolesRaw = person.getMeta(ROLES_KEY);
+        setMeta(person, ROLES_KEY, String.format("%s###%s", rolesRaw, role));
     }
 
 }

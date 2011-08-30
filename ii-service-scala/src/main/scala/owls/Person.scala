a9bed7275f69ca4b784fbb952d00b1c3d1b27388
Anton Chebotaev <anton.chebotaev@gmail.com>
2011-08-30 18:29:56 +0400

    Person.scala added to demonstrate @BeanProperty annotation

--- ii-service-scala/src/main/scala/owls/Person.scala
+++ ii-service-scala/src/main/scala/owls/Person.scala
@@ -1,11 +1,17 @@
 package owls
 
+import reflect.{BooleanBeanProperty, BeanProperty}
+
 /**
  * @author Anton Chebotaev
  *         Owls Proprietary
  */
-
-
 class Person {
+
+  @BeanProperty // this will generate POJO-style getter and setter for java
+  var name = "Person name"
+
+  @BooleanBeanProperty // this will generate POJO-style boolean getter and setter for java
+  val badass = true;
 
 }
